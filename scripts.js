fa3da9ba516fd0a360409376fc7f2b1fad4d577c
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-15 19:39:49 -0300

    Criando a li

--- scripts.js
+++ scripts.js
@@ -38,7 +38,9 @@ form.onsubmit = (event) => {
 
 function expenseAdd(newExpense) {
     try {
-
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+        
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas")
         console.log(error)
