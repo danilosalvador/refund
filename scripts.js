3f2dade2f6f96b0b40d9cf9d3427091f15127983
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-15 19:18:07 -0300

    Aceitando somente números no valor

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const amount = document.getElementById("amount")
 
 amount.oninput = () => {
-    
+    const numberRegex = /\D/g
+    let value = amount.value.replace(numberRegex, "")
+
+    amount.value = value
 }
