7836f5bcc3aa49f3ba0565d512552f390e89d812
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-15 19:24:17 -0300

    Formatando a moeda

--- scripts.js
+++ scripts.js
@@ -4,5 +4,16 @@ amount.oninput = () => {
     const numberRegex = /\D/g
     let value = amount.value.replace(numberRegex, "")
 
-    amount.value = value
+    value = value / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    const valueFormatted = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return valueFormatted;
 }
